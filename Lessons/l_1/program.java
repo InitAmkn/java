96d801145f85b61333a5757fc888ce15d0a7fb92
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-11-21 20:03:03 +0300

    first commit

---
+++ Lessons/l_1/program.java
@@ -0,0 +1,17 @@
+package Lessons.l_1;
+
+import java.io.Console;
+
+/**
+ * program
+ */
+
+public class program {
+
+    public static void main(String[] args) {
+        String s = "x";
+        System.out.println(s);
+        var op = 123;
+        System.out.println(op);
+    }
+}
