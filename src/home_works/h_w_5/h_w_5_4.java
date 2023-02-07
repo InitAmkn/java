40ff1630ec505f38e75ae714aa97e2504c2c3ca6
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-02-07 20:04:21 +0300

    add home work 5, ex. 1-4

--- src/home_works/h_w_5/h_w_5_4.java
+++ src/home_works/h_w_5/h_w_5_4.java
@@ -10,68 +10,85 @@ public class h_w_5_4 {
          * 4) *На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
          */
         Integer[][] map = new Integer[8][8];
-        fillingFirstQueen(map);
-        fillingOtherQueen(map, 7);
+        fillingMap(map);
+        fillingQueens(map);
         printMap(map);
     }
 
-    public static void fillingFirstQueen(Integer[][] map) {
-
+    public static void fillingMap(Integer[][] map) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
 
                 map[i][j] = 0;
             }
-
         }
     }
 
-    public static void fillingOtherQueen(Integer[][] map, int countQueen) {
-
-        for (int x = 1; x < map.length; x++) {
-            for (int y = 0; y < map[x].length; y++) {
-                if (map[x][y] == 0)
-                    putQueen(x, y, map);
+    public static int QueensCounter(Integer[][] map) {
+        int count = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] != 0)
+                    count++;
             }
         }
-
-        // if (countQueen > 0)
-        // fillingOtherQueen(map, countQueen - 1);
+        return count;
     }
 
-    public static Integer[][] putQueen(int x, int y, Integer[][] map) {
+    public static void fillingQueens(Integer[][] map) {
+
+        Random r = new Random();
+        int x = r.nextInt(map.length);
+        int y = r.nextInt(map.length);
+        map[x][y] = 1;
+        while (QueensCounter(map) < map.length) {
+            int i = r.nextInt(map.length);
+            int j = r.nextInt(map.length);
+            if (isPossiblePut(i, j, map)) {
+                map[i][j] = 1;
+            }
+            if (i == map.length - 1 && j == map[i].length - 1) {
+                if (QueensCounter(map) < map.length) {
+                    fillingMap(map);
+                }
+            }
+        }
+    }
+
+    public static boolean isPossiblePut(int x, int y, Integer[][] map) {
         for (int i = 0; i < map.length; i++) {
             if (i + x < map.length && i + y < map.length) {
-                if (map[i + x][i + y] == 0) {
-                    map[i + x][i + y] = 2;
+                if (map[i + x][i + y] != 0) {
+                    return false;
                 }
             }
             if (x - i >= 0 && y - i >= 0) {
-                if (map[x - i][y - i] == 0) {
-                    map[x - i][y - i] = 2;
+                if (map[x - i][y - i] != 0) {
+                    return false;
                 }
             }
 
             if (i + x < map.length && y - i >= 0) {
-                if (map[i + x][y - i] == 0) { // map[i + x][y - i] = 2;
-                    map[i + x][y - i] = 2;
+                if (map[i + x][y - i] != 0) {
+                    return false;
                 }
             }
             if (x - i >= 0 && i + y < map.length) {
-                if (map[x - i][i + y] == 0) {
-                    map[x - i][i + y] = 2;
+                if (map[x - i][i + y] != 0) {
+                    return false;
                 }
             }
 
-            if (map[x][i] == 0) {
-                map[x][i] = 2;
+            if (map[x][i] != 0) {
+                return false;
             }
-            if (map[i][y] == 0) {
-                map[i][y] = 2;
+            if (map[i][y] != 0) {
+                return false;
+
             }
-            map[x][y] = 1;
+
         }
-        return map;
+        return true;
 
     }
 
